c7fff433814b9cafa0364ef6c4b27b0bff8c82b3
Zxynx <103110232+Zxynx@users.noreply.github.com>
2022-07-31 21:50:13 +0700

    Update RyzMain.py

--- RyzMain.py
+++ RyzMain.py
@@ -20,7 +20,7 @@ def skip_mission(token, rec):
 			js = json.loads(response)
 			logs = js['data']['Logs']
 			cash = logs[len(logs)-1]['Message'].split()[5]
-			print(f'(`[ ${moment().format('HH:mm:ss')} ] ✅SUCCESS|Script By: Rian Febriansyah|Ryz Clone Acc|Your Money -> {cash}')
+			print(f'(`[ ${moment().format('HH:mm:ss')} ] ✅SUCCESS|Script By : Ryz_Wp|Your Money -> {cash}')
 		except Exception as e:
 			pass
 			
